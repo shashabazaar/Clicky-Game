02798640f0418d56957ebe40f552df2fd7079086
shashabazaar <shafiqah.sharif@yahoo.com>
2018-11-27 11:33:43 -0500

    Updates

---
+++ precache-manifest.7bff9edf1f3947fb7f6af855b5ea39e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "e3effb9c519edebe769a",
+    "url": "/Clicky-Game/static/js/main.e3effb9c.chunk.js"
+  },
+  {
+    "revision": "a1f95f66e8fd6e86deee",
+    "url": "/Clicky-Game/static/js/1.a1f95f66.chunk.js"
+  },
+  {
+    "revision": "e3effb9c519edebe769a",
+    "url": "/Clicky-Game/static/css/main.842a138e.chunk.css"
+  },
+  {
+    "revision": "61105bb6294ebd2f2b07a0f8bb1e1333",
+    "url": "/Clicky-Game/index.html"
+  }
+];
